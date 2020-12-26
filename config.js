b818c79208e68aa0f51c50bf13926b01371c75f3
sjagoori <sjagoori@users.noreply.github.com>
2020-12-26 13:44:52 +0100

    Added dburl, ref. https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import firebase from 'firebase'
 const config = {
   projectId: process.env.projectId,
   apiKey: process.env.apiKey,
-  databaseURL: process.env.databaseURL
+  databaseURL: "https://biopic-e818a-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 // Make sure there isnt already an existing firebaseApp
